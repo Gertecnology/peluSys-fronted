6efc7f4975b7f82b3b557361f8d3b357e83a2cb6
EmilianoChavezDev <eemirandachavez@gmail.com>
2023-04-17 20:57:18 -0400

    subida de layout primera version

---
+++ layout/Layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head"
+import Sidebar from "../components/Sidebar"
+import Header from "../components/Header"
+import styles from "../src/styles/Layout.module.css"
+
+const Layout = ({ children, pagina }) => {
+  return (
+    <>
+      <Head>
+        <title>CRUD - {pagina}</title>
+      </Head>
+      <div className="md:flex">
+        <aside className="md:w-3/12 bg-blueEdition">
+          <Sidebar />
+        </aside>
+
+        <div className="md:w-9/12 ">
+          <div className="grid grid-flow-row-2">
+            <div className= {`${styles.border}`} >
+              <Header />
+            </div>
+            <main className="h-screen ">
+              {children}
+            </main>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Layout
